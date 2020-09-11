ff8bb1654067973c89f9af3679d1caa007545025
Nick Escalona <nicholas.escalona@gmail.com>
2020-09-11 01:31:26 -0500

    .

--- demo-assignment-1-classlib/scripts/grade.sh
+++ demo-assignment-1-classlib/scripts/grade.sh
@@ -12,7 +12,7 @@ dotnet test Prime.Grader | tee results.txt
 fail=`egrep -i "failed|error" results.txt | wc -l`
 
 coverage=`sed -n 2p Prime.Tests/*/*/coverage.*.xml | cut -d'"' -f2`
-percentage=`bc <<< "scale=2; ($coverage * 100) / 1"`
+percentage=`awk -vc=$coverage 'BEGIN{printf "%.2f" ,c * 100}'`
 
 if [ -n "$coverage" ]; then
   echo "" >> results.txt
@@ -27,15 +27,17 @@ fi
 
 echo "-------------------------------------------------------" >> $vocareumReportFile
 
+maxscore=5
 if [ "$fail" == "0" ]; then
   score=5
-  echo "score", $score >> $vocareumGradeFile
   echo "Your program meets all requirements." >> $vocareumReportFile
 else
   score=0
-  echo "score", $score >> $vocareumGradeFile
+  echo "Score: ${score}/${maxscore}" >> $vocareumReportFile
   echo "Your program does not meet all requirements." >> $vocareumReportFile
 fi
+echo "Score: ${score}/${maxscore}" >> $vocareumReportFile
+echo "score", $score >> $vocareumGradeFile
 
 echo "-------------------------------------------------------" >> $vocareumReportFile
 echo "" >> $vocareumReportFile
@@ -48,4 +50,4 @@ curl -d "{\"score\":$score}" \
 -H "Content-Type: application/json" \
 -H "EXTERNAL_CLIENT_ID: Vk9DQVJFVU1fTEFC" \
 -H "EXTERNAL_CLIENT_TOKEN: ODkyNGRhYWQ1MzBkNzJlYWNhNzJkZTNjMzBjYzhhZWM" \
--X PUT  https://app-ms.revature.com/apigateway/lab/vocareum/users/$VOC_USERID/parts/$VOC_PARTID >> $vocareumReportFile
+-X PUT https://app-ms.revature.com/apigateway/lab/vocareum/users/$VOC_USERID/parts/$VOC_PARTID >> $vocareumReportFile
