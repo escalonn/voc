56b823210188d50969279a2f9c427b9debfa5f04
Nick Escalona <nicholas.escalona@gmail.com>
2020-09-11 01:36:56 -0500

    .

--- demo-assignment-2-webapi/scripts/grade.sh
+++ demo-assignment-2-webapi/scripts/grade.sh
@@ -19,15 +19,17 @@ fail=`egrep -i "failed|error" results.txt | wc -l`
 
 echo "-------------------------------------------------------" >> $vocareumReportFile
 
+maxscore=5
 if [ "$fail" == "0" ]; then
   score=5
-  echo "score", $score >> $vocareumGradeFile
   echo "Your program meets all requirements." >> $vocareumReportFile
 else
   score=0
-  echo "score", $score >> $vocareumGradeFile
+  echo "Score: ${score}/${maxscore}" >> $vocareumReportFile
   echo "Your program does not meet all requirements." >> $vocareumReportFile
 fi
+echo "Score: ${score}/${maxscore}" >> $vocareumReportFile
+echo "score", $score >> $vocareumGradeFile
 
 echo "-------------------------------------------------------" >> $vocareumReportFile
 echo "" >> $vocareumReportFile
@@ -40,4 +42,4 @@ curl -d "{\"score\":$score}" \
 -H "Content-Type: application/json" \
 -H "EXTERNAL_CLIENT_ID: Vk9DQVJFVU1fTEFC" \
 -H "EXTERNAL_CLIENT_TOKEN: ODkyNGRhYWQ1MzBkNzJlYWNhNzJkZTNjMzBjYzhhZWM" \
--X PUT  https://app-ms.revature.com/apigateway/lab/vocareum/users/$VOC_USERID/parts/$VOC_PARTID >> $vocareumReportFile
+-X PUT https://app-ms.revature.com/apigateway/lab/vocareum/users/$VOC_USERID/parts/$VOC_PARTID >> $vocareumReportFile
--- demo-assignment-3-mvc/scripts/grade.sh
+++ demo-assignment-3-mvc/scripts/grade.sh
@@ -18,15 +18,17 @@ fi
 
 echo "-------------------------------------------------------" >> $vocareumReportFile
 
+maxscore=5
 if [ "$fail" == "0" ]; then
   score=5
-  echo "score", $score >> $vocareumGradeFile
   echo "Your program meets all requirements." >> $vocareumReportFile
 else
   score=0
-  echo "score", $score >> $vocareumGradeFile
+  echo "Score: ${score}/${maxscore}" >> $vocareumReportFile
   echo "Your program does not meet all requirements." >> $vocareumReportFile
 fi
+echo "Score: ${score}/${maxscore}" >> $vocareumReportFile
+echo "score", $score >> $vocareumGradeFile
 
 echo "-------------------------------------------------------" >> $vocareumReportFile
 echo "" >> $vocareumReportFile
@@ -39,4 +41,4 @@ curl -d "{\"score\":$score}" \
 -H "Content-Type: application/json" \
 -H "EXTERNAL_CLIENT_ID: Vk9DQVJFVU1fTEFC" \
 -H "EXTERNAL_CLIENT_TOKEN: ODkyNGRhYWQ1MzBkNzJlYWNhNzJkZTNjMzBjYzhhZWM" \
--X PUT  https://app-ms.revature.com/apigateway/lab/vocareum/users/$VOC_USERID/parts/$VOC_PARTID >> $vocareumReportFile
+-X PUT https://app-ms.revature.com/apigateway/lab/vocareum/users/$VOC_USERID/parts/$VOC_PARTID >> $vocareumReportFile
